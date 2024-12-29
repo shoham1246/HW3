a042025b85b3888c9100fb9931640eb025bbc967
Mamat <orsimchy100@gmail.com>
2024-12-29 18:28:46 +0200

    or update more fields

--- stack.c
+++ stack.c
@@ -21,18 +21,77 @@
  * @return Non-zero value in case of an error
  */
 
+
+//USER FUNCTIONS
+typedef void (*print_t)(elem_t e); //function that gets element pointer and print.
+
+
 Stack stack_create(int max_size) {
-	Stack result = malloc(sizeof(elem_t) * max_size);
+    Stack result = malloc(sizeof(struct stack));
 	if (result==NULL) {
 		return NULL;
 	}
 	//need fields?
+
+	//initialize fields
+	result->max_size = max_size;
+	result->num_of_elements = 0;
+	result->head_elem = NULL;
+	result->elements = malloc(sizeof(elem_t) * max_size); 
+
+	/*
+		Stack strucure:
+		-we have an array of elements[max_size]
+		-the start of the array is the bottom of the stack
+	*/
+
+	if (result->elements == NULL) {
+        free(result);
+        return NULL;
+    }
+
+
 	return result;
+}
+
+
+int stack_size(Stack stack){
+	int stack_size = stack->num_of_elements;
+	return stack_size;
+}
+
+bool stack_is_empty(Stack stack){
+	if(stack->num_of_elements == 0){
+		return TRUE;
+	}else{
+		return FALSE;
+	}
+}
+
+int stack_capacity(Stack stack){
+	int capacity = stack->max_size - stack->num_of_elements;
+	return capacity;
+}
+
+
+void stack_print(Stack stack){
+
+	int num_of_elements = stack->num_of_elements;
+
+	/*
+		we print from the top element to the bottom,
+		the top is at the end of the array, which is why
+		the loop is from end to start of the array
+	*/
+	for(int i = num_of_elements -1 ; i>=0 ; i--){
+		print_t(stack->elements[i]);
+	}
 }
 
 
 
 int main(int argc, char **argv) {
+
 	printf("Hello World\n");
 	return 0;
 }
--- stack.h
+++ stack.h
@@ -8,17 +8,28 @@
 
 enum result { SUCCESS = 0, FAIL };
 typedef void * elem_t;
-typedef struct stack* Stack;
+
+typedef struct stack {
+	int max_size;
+	int num_of_elements;
+	void* head_elem; //pointer to the top element
+	elem_t* elements; // Array to store the elements
+} *Stack;
 
 Stack stack_create(int max_size); //complete alone??
-int stack_destroy(Stack stack);
 
+int stack_destroy(Stack stack);
 int stack_push(Stack stack, void * elem_t);
 void stack_pop(Stack stack);
 void * stack_peek(Stack stack);
+
 int stack_size(Stack stack);
 bool stack_is_empty(Stack stack);
 int stack_capacity(Stack stack);
 void stack_print(Stack stack);
+
+
+
+
 #endif /* COMMON_H */
 
